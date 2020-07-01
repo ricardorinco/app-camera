c250b119d8460b59cded56e4b65ef26cf56ca482
Ricardo Rinco <ricardo.rinco@hotmail.com>
2020-07-01 15:51:48 -0300

    Criação do projeto com ngx-webcam

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { WebcamModule } from 'ngx-webcam';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    WebcamModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
